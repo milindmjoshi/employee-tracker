3f816e9b27f7f97401593219a41f296201ed44e3
Milman <milindmjoshi@live.com>
2024-03-14 21:18:37 -0400

    Get view roles, view departments, and view employees working

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+// Import and require mysql2
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // TODO: Add MySQL password here
+    password: 'password',
+    database: 'employee_db'
+  },
+  console.log(`Connected to the employee_db database.`)
+);
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const db = require("./db/connection");
+const { printTable } = require('console-table-printer');
 
 
 inquirer.prompt(
@@ -14,4 +16,66 @@ inquirer.prompt(
     }]
 ).then((answers)=>{
     console.log(answers);
+    switch(answers.menuChoice){
+        case "View All Departments":
+            console.log("View Departments Selected");
+            viewDepartments();
+            break;
+        case "View All Roles":
+            console.log("View All Roles selected");
+            viewRoles();
+            break;
+        case "View All Employees":
+            console.log("View all Employees selected");
+            viewEmployees();
+            break;
+        default:
+            console.log("Invalid selection");
+    }
 })
+
+function viewDepartments(){
+    const SQL = "SELECT id,name FROM DEPARTMENT";
+    db.query(SQL, (err, rows, fields) => {
+        if (err instanceof Error) {
+          console.log(err);
+          return;
+        }
+        // console.log(rows);
+        // console.log(fields);
+        printTable(rows);
+      });
+}
+
+function viewRoles(){
+    const SQL = "SELECT role.id, role.title, role.salary, department.name FROM role " + 
+    "JOIN department " + 
+    "ON role.department_id = department.id;";
+    db.query(SQL, (err, rows, fields) => {
+        if (err instanceof Error) {
+          console.log(err);
+          return;
+        }
+        // console.log(rows);
+        // console.log(fields);
+        printTable(rows);
+      });
+}
+
+function viewEmployees(){
+    const SQL = "SELECT employee.id, employee.first_name, employee.last_name, role.title,role.salary, department.name AS 'dept name' " +  
+    " FROM employee " + 
+    " JOIN role " + 
+    " ON employee.role_id = role.id " + 
+    " JOIN department " + 
+    " ON role.department_id = department.id;";
+    db.query(SQL, (err, rows, fields) => {
+        if (err instanceof Error) {
+          console.log(err);
+          return;
+        }
+        // console.log(rows);
+        // console.log(fields);
+        printTable(rows);
+      });
+}
